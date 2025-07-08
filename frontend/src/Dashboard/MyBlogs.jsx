03ca449f976741fc126fa190dabe3b996d815dfa
Student Shaswat Gautam <shaswat2016@gmail.com>
2025-07-09 01:43:58 +0530

    a

--- frontend/src/Dashboard/MyBlogs.jsx
+++ frontend/src/Dashboard/MyBlogs.jsx
@@ -2,6 +2,7 @@ import axios from "axios";
 import React, { useEffect, useState } from "react";
 import toast from "react-hot-toast";
 import { Link, useNavigate } from "react-router-dom";
+import { BACKEND_URL } from "../../utils";
 
 function MyBlogs() {
   const navigateTo = useNavigate()
@@ -10,7 +11,7 @@ function MyBlogs() {
     const fetchMyBlogs = async () => {
       try {
         const { data } = await axios.get(
-          "http://localhost:4001/api/blogs/my-blog",
+         `${BACKEND_URL}/api/blogs/my-blog`,
           { withCredentials: true }
         );
         console.log(data);
